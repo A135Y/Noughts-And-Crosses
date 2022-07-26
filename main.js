1ffa1f40d8db7436594eb836292a3c4c55392b24
Abdalla Yusuf <me@a135y.com>
2022-07-26 19:59:33 +0100

    Layout Fix

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //Variables
 const start = document.getElementById(start);
 const reset = document.getElementById(reset);
-
-
+const turn = document.getElementById(Whos-Turn);
+const block = document.getElementsByClassName(block);
 
 //Buttons Logic
 
